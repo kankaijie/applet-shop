1248e4c7daf9f5663ee3f286b4631a26c59c0f60
2798605887@qq.com <2798605887@qq.com>
2019-12-06 18:17:53 +0800

    提交

--- shop-goods/src/main/java/com/org/shop/client/UserClient.java
+++ shop-goods/src/main/java/com/org/shop/client/UserClient.java
@@ -6,6 +6,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@FeignClient(value = "shop-user",contextId = "")
+@FeignClient(value = "shop-user",contextId = "u1")
 public interface UserClient extends UserApi {
 }
--- shop-zuul/src/main/java/com/org/shop/ShopZuulApplication.java
+++ shop-zuul/src/main/java/com/org/shop/ShopZuulApplication.java
@@ -1,11 +1,16 @@
 package com.org.shop;
-
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+@EnableZuulProxy
 @EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ShopZuulApplication {
 
