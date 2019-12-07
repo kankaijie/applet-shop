53918f36a34ced0fcb4625a36f30df544a35a2ea
2798605887@qq.com <2798605887@qq.com>
2019-12-07 15:52:04 +0800

    添加auth认证模板

---
+++ shop-auth/src/main/java/com/org/shop/ShopAuthApplication.java
@@ -0,0 +1,23 @@
+package com.org.shop;
+
+import lombok.extern.java.Log;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@Log
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+@MapperScan(basePackages = "com.org.shop")
+public class ShopAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopAuthApplication.class, args);
+    }
+
+}
